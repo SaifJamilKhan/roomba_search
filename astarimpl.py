15105d0c17b6bc4c9f5774dc66e45ead084432fe
Chenlei Shen <shenchenlei@gmail.com>
2018-11-22 10:47:54 -0500

    add heuristic options

--- astarimpl.py
+++ astarimpl.py
@@ -1,15 +1,16 @@
 import heapq
+import math
 
 class PriorityQueue:
     def __init__(self):
         self.elements = []
-    
+
     def empty(self):
         return len(self.elements) == 0
-    
+
     def put(self, item, priority):
         heapq.heappush(self.elements, (priority, item))
-    
+
     def get(self):
         return heapq.heappop(self.elements)[1]
 
@@ -18,14 +19,14 @@ class SquareGrid:
         self.width = width
         self.height = height
         self.walls = []
-    
+
     def in_bounds(self, id):
         (x, y) = id
         return 0 <= x < self.width and 0 <= y < self.height
-    
+
     def passable(self, id):
         return id not in self.walls
-    
+
     def neighbors(self, id):
         (x, y) = id
         results = [(x+1, y), (x, y-1), (x-1, y), (x, y+1)]
@@ -34,12 +35,19 @@ class SquareGrid:
         results = filter(self.passable, results)
         return results
 
-def heuristic(a, b):
+def heuristic(a, b, option):
+    distance = None
     (x1, y1) = a
     (x2, y2) = b
-    return abs(x1 - x2) + abs(y1 - y2)
+    if option == 'manhattan':
+        distance = abs(x1 - x2) + abs(y1 - y2)
+    elif option == 'euclidean':
+        distance = math.sqrt(pow((x1 - x2),2) + pow((x1 - x2),2))
+    elif option == 'diagonal':
+        distance = max(abs(x1 - x2), abs((x1 - x2)))
+    return distance
 
-def a_star_search(graph, start, goal):
+def a_star_search(graph, start, goal, option):
     queue = PriorityQueue()
     queue.put(start, 0)
     came_from = {}
@@ -49,23 +57,22 @@ def a_star_search(graph, start, goal):
     min_distance = 9999999
     while not queue.empty():
         current = queue.get()
-        
+
         if current == goal:
 
             for next in graph.neighbors(current): # look at cost of all the neighbors
                 if next in cost_so_far:
                     min_distance = min(min_distance, cost_so_far[next])
             break
-        
+
         for next in graph.neighbors(current):
             new_cost = cost_so_far[current] + graph.cost(current, next)
             if next not in cost_so_far or new_cost < cost_so_far[next]:
                 cost_so_far[next] = new_cost
-                priority = new_cost + heuristic(goal, next)
+                priority = new_cost + heuristic(goal, next, option)
                 queue.put(next, priority)
                 came_from[next] = current
 
     if min_distance == 9999999:
         min_distance = 0
     return came_from, cost_so_far, min_distance
-
--- roomba_search.py
+++ roomba_search.py
@@ -4,10 +4,9 @@ from random import randint
 class GridGivenSize(SquareGrid):
     def __init__(self, width, height):
         super().__init__(width, height)
-    
+
     def cost(self, from_node, to_node):
         return 1 # cost can be made variable but cost of 1 is assigned as of now
-
 
 def draw_square(graph, id, style, width):
     r = "."
@@ -23,34 +22,48 @@ def draw_room(graph, width=2, **style):
             print("%%-%ds" % width % draw_square(graph, (x, y), style, width), end="")
         print()
 
-
 def generateRoom(numberOfwalls):
-	room = GridGivenSize(10, 10)
+    room = GridGivenSize(10, 10)
 
-	walls = []
-	for x in range(numberOfwalls):
-		walls.append((randint(0, 9),randint(0, 9)))
+    walls = []
+    for x in range(numberOfwalls):
+        walls.append((randint(0, 9),randint(0, 9)))
 
-	room.walls = walls
+    room.walls = walls
 
-	start, goal = (randint(0, 9),randint(0, 9)), (randint(0, 9),randint(0, 9))
-	came_from, cost_so_far, min_distance = a_star_search(room, start, goal)
-	print("Min distance is : ")
-	print(min_distance)
-	draw_room(room, width=3, number=cost_so_far, start=start, goal=goal)
-	print()
-	return min_distance
+    start, goal = (randint(0, 9),randint(0, 9)), (randint(0, 9),randint(0, 9))
+    came_from, cost_so_far, min_distance = a_star_search(room, start, goal, 'manhattan')
+    came_from, cost_so_far, eu_min_distance = a_star_search(room, start, goal, 'euclidean')
+    came_from, cost_so_far, dia_min_distance = a_star_search(room, start, goal, 'diagonal')
+    print("[manhattan] Min distance is : ", min_distance)
+    print("[euclidean] Min distance is : ", eu_min_distance)
+    print("[diagonal] Min distance is : ", dia_min_distance)
+    # draw_room(room, width=3, number=cost_so_far, start=start, goal=goal)
+    # print()
+    return [min_distance, eu_min_distance, dia_min_distance]
 
-	
+
 overall_average = 0
 count = 0
+overall_average_eu = 0
+count_eu = 0
+overall_average_dia = 0
+count_dia = 0
 for x in range(10000): #create 100 rooms with 10 walls in them each
-	shortestDistance = generateRoom(40) 
-	if shortestDistance != 0:
-		count += 1
-		overall_average += shortestDistance
+    shortestDistance, shortestDistance_eu, shortestDistance_dia = generateRoom(40)
+    if shortestDistance != 0:
+        count += 1
+        overall_average += shortestDistance
+    if shortestDistance_eu != 0:
+        count_eu += 1
+        overall_average_eu += shortestDistance_eu
+    if shortestDistance_dia != 0:
+        count_dia += 1
+        overall_average_dia += shortestDistance_dia
 
 overall_average = (overall_average / count)
-print("Average distance is : ")
-print(overall_average)
-
+overall_average_eu = (overall_average_eu / count_eu)
+overall_average_dia = (overall_average_dia / count_dia)
+print("[manhattan] Average distance is : ", overall_average)
+print("[euclidean] Average distance is : ", overall_average_eu)
+print("[diagonal] Average distance is : ", overall_average_dia)
