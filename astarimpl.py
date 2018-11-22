39944629bf0004929061df1759d7e0148eb73414
Saif Khan <sj5khan@uwaterloo.ca>
2018-11-21 20:11:50 -0500

    Average calculator

--- astarimpl.py
+++ astarimpl.py
@@ -34,55 +34,27 @@ class SquareGrid:
         results = filter(self.passable, results)
         return results
 
-
-class GridWithWeights(SquareGrid):
-    def __init__(self, width, height):
-        super().__init__(width, height)
-        self.weights = {}
-    
-    def cost(self, from_node, to_node):
-        return self.weights.get(to_node, 1)
-
 def heuristic(a, b):
     (x1, y1) = a
     (x2, y2) = b
     return abs(x1 - x2) + abs(y1 - y2)
 
-
-def draw_tile(graph, id, style, width):
-    r = "."
-    if 'number' in style and id in style['number']: r = "%d" % style['number'][id]
-    if 'point_to' in style and style['point_to'].get(id, None) is not None:
-        (x1, y1) = id
-        (x2, y2) = style['point_to'][id]
-        if x2 == x1 + 1: r = ">"
-        if x2 == x1 - 1: r = "<"
-        if y2 == y1 + 1: r = "v"
-        if y2 == y1 - 1: r = "^"
-    if 'start' in style and id == style['start']: r = "A"
-    if 'goal' in style and id == style['goal']: r = "Z"
-    if 'path' in style and id in style['path']: r = "@"
-    if id in graph.walls: r = "#" * width
-    return r
-
-def draw_grid(graph, width=2, **style):
-    for y in range(graph.height):
-        for x in range(graph.width):
-            print("%%-%ds" % width % draw_tile(graph, (x, y), style, width), end="")
-        print()
-
 def a_star_search(graph, start, goal):
-    frontier = PriorityQueue()
-    frontier.put(start, 0)
+    queue = PriorityQueue()
+    queue.put(start, 0)
     came_from = {}
     cost_so_far = {}
     came_from[start] = None
     cost_so_far[start] = 0
-    
-    while not frontier.empty():
-        current = frontier.get()
+    min_distance = 9999999
+    while not queue.empty():
+        current = queue.get()
         
         if current == goal:
+
+            for next in graph.neighbors(current): # look at cost of all the neighbors
+                if next in cost_so_far:
+                    min_distance = min(min_distance, cost_so_far[next])
             break
         
         for next in graph.neighbors(current):
@@ -90,10 +62,12 @@ def a_star_search(graph, start, goal):
             if next not in cost_so_far or new_cost < cost_so_far[next]:
                 cost_so_far[next] = new_cost
                 priority = new_cost + heuristic(goal, next)
-                frontier.put(next, priority)
+                queue.put(next, priority)
                 came_from[next] = current
-    
-    return came_from, cost_so_far
+
+    if min_distance == 9999999:
+        min_distance = 0
+    return came_from, cost_so_far, min_distance
 
 
 # diagram4.weights = {loc: 5 for loc in [(3, 4), (3, 5), (4, 1), (4, 2),
--- roomba_search.py
+++ roomba_search.py
@@ -1,14 +1,57 @@
 from astarimpl import *
-diagram4 = GridWithWeights(10, 10)
-diagram4.walls = [
-(2,2),(1,2),(0,2),(3,2),(4,2),(5,2),
-(10,4),(9,4),(8,4),(7,4),(6,4),(3,4),(4,4),(5,4),
-(2,6),(1,6),(0,6),(3,6),(4,6),(5,6)
-]
+from random import randint
 
-start, goal = (1, 1), (7, 8)
-came_from, cost_so_far = a_star_search(diagram4, start, goal)
-draw_grid(diagram4, width=3, point_to=came_from, start=start, goal=goal)
-print()
-draw_grid(diagram4, width=3, number=cost_so_far, start=start, goal=goal)
+class GridGivenSize(SquareGrid):
+    def __init__(self, width, height):
+        super().__init__(width, height)
+    
+    def cost(self, from_node, to_node):
+        return 1 # cost can be made variable but cost of 1 is assigned as of now
+
+
+def draw_square(graph, id, style, width):
+    r = "."
+    if 'number' in style and id in style['number']: r = "%d" % style['number'][id]
+    if 'start' in style and id == style['start']: r = "A"
+    if 'goal' in style and id == style['goal']: r = "Z"
+    if 'path' in style and id in style['path']: r = "@"
+    if id in graph.walls: r = "#" * width
+    return r
+
+def draw_room(graph, width=2, **style):
+    for y in range(graph.height):
+        for x in range(graph.width):
+            print("%%-%ds" % width % draw_square(graph, (x, y), style, width), end="")
+        print()
+
+
+def generateRoom(numberOfwalls):
+	room = GridGivenSize(10, 10)
+
+	walls = []
+	for x in range(numberOfwalls):
+		walls.append((randint(0, 9),randint(0, 9)))
+
+	room.walls = walls
+
+	start, goal = (randint(0, 9),randint(0, 9)), (randint(0, 9),randint(0, 9))
+	came_from, cost_so_far, min_distance = a_star_search(room, start, goal)
+	print("Min distance is : ")
+	print(min_distance)
+	draw_room(room, width=3, number=cost_so_far, start=start, goal=goal)
+	print()
+	return min_distance
+
+	
+overall_average = 0
+count = 0
+for x in range(100): #create 100 rooms with 10 walls in them each
+	shortestDistance = generateRoom(20) 
+	if shortestDistance != 0:
+		count += 1
+		overall_average += shortestDistance
+
+overall_average = (overall_average / count)
+print("Average distance is : ")
+print(overall_average)
 
